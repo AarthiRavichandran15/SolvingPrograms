1fcd3eeccaf7e8c12954187dca0cea26738fcf2d
AarthiRavichandran15 <aarthiravichandran1503@gmail.com>
2023-09-16 20:35:54 +0530

    Even and odd

---
+++ eo.java
@@ -0,0 +1,23 @@
+public class Ajay
+{
+	public static void main(String[] args)
+	{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter a value");
+	int number=sc.nextInt();
+	Ajay aj=new Ajay();
+	aj.Solution(number);
+	}
+	public int Solution(int number) {
+	if(number%2==0)
+	{
+		System.out.println("Even");
+	}
+	else if(number%1==0)
+	{
+		System.out.println("Odd");
+	}
+	return number;
+	}
+}
+	
